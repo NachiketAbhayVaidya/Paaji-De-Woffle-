480ce550dbfb604583770321342225fb9c4388cc
NachiketAbhayVaidya <vaidyanachiket788@gmail.com>
2024-09-19 17:45:59 +0530

    Modification of website

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-let slide=document.querySelectorAll(".slide");
-let productdetail = document.querySelector(".pro1");
-slide.forEach((button) =>{
-    button.addEventListener("click",()=>{
-        console.log("button was clicked")
-        productdetail.innerText = "pro2" 
-   
-    });
-})
+const icon = document.querySelector(".ri-menu-3-line")
+
+
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicButtons:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+      0: {
+        slidesPreView: 1
+      },
+      768:{
+        slidesPreView: 2
+      },
+      1024:{
+        slidesPreView: 3
+      }
+    }
+  
+  });
 
     
+
+icon.addEventListener('click',() =>{
+  icon.innerText = "about"
+})
 
